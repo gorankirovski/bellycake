726f231016326698180c6fe4032637de40317e19
motikolorado <meetgoran@gmail.com>
2023-05-03 11:23:42 -0500

    yup

--- frontend/src/components/layout/Loader/Loader.jsx
+++ frontend/src/components/layout/Loader/Loader.jsx
@@ -1,8 +1,26 @@
 import React from "react";
+// import Typed from "react-typed";
 import "./Loader.css";
 const Loader = () => {
+  // set up the options object
+  // const options = {
+  //   strings: [
+  //           "Loading..."
+  //       ],
+  //   typeSpeed: 50,
+  //   backSpeed: 30,
+    // backDelay: 5000,
+    // loop: true,
+    // showCursor: true,
+    // cursorChar: "|",
+    // smartBackspace: true,
+    // cursorBlinking: true,
+    // you can also set a custom blinking rate in ms
+    // cursorBlinkSpeed: 500
+  // };
   return (
     <div className="loaderBG">
+      {/* <span><Typed {...options} /></span> */}
       <div className="wrapper">
         <div className="circle"></div>
         <div className="circle"></div>
@@ -10,7 +28,6 @@ const Loader = () => {
         <div className="shadow"></div>
         <div className="shadow"></div>
         <div className="shadow"></div>
-        <span>please wait a few sec.. :)</span>
       </div>
     </div>
   );
